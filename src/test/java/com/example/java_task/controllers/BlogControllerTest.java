b6f2d4e8721f4407a572d5678f7d28081498bf2c
Nuriddin <inoyatovnuriddin009@gmail.com>
2023-07-30 22:33:47 +0500

    task finished

--- src/test/java/com/example/java_task/controllers/BlogControllerTest.java
+++ src/test/java/com/example/java_task/controllers/BlogControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class BlogControllerTest {
 
     @Autowired
--- src/test/java/com/example/java_task/controllers/CommentControllerTest.java
+++ src/test/java/com/example/java_task/controllers/CommentControllerTest.java
@@ -29,7 +29,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class CommentControllerTest {
 
     @Mock
